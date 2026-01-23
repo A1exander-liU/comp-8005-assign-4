cd165e5d0302bd0b3c10a9238f7b23f8b06f960b
A1exander-liU <alexanderliu2016@gmail.com>
2026-01-23 15:56:45 -0800

    feat: add password cracking

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/A1exander-liU/comp-8005-assign-1
 go 1.25.1
 
 require (
+	github.com/go-crypt/crypt v0.4.7
+	go.uber.org/zap v1.27.1
+)
+
+require (
+	github.com/go-crypt/x v0.4.9 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.27.1 // indirect
+	golang.org/x/sys v0.38.0 // indirect
 )
--- utils.go
+++ utils.go
@@ -6,8 +6,11 @@
 package utils
 
 import (
+	"errors"
 	"net"
 	"strconv"
+
+	"github.com/go-crypt/crypt"
 )
 
 type ShadowData struct {
@@ -33,4 +36,47 @@ func ParseAddress(ip string, port int) string {
 	}
 
 	return net.JoinHostPort(parsedIP.String(), strconv.Itoa(port))
+}
+
+func checkPassword(decoder *crypt.Decoder, plaintext, hash string) bool {
+	digest, _ := decoder.Decode(hash)
+	return digest.Match(plaintext)
+}
+
+// CrackPassword tries all permutations of passwords up to `maxLength`. Returns
+// the plaintext if crack was successful otherwise it will return an error.
+//
+// The total characte set includes:
+//
+// - alphanumeric characters (a-z, A-Z, 0-9)
+//
+// - special characters: @ # % ^ & * ( ) _ + - = . , : ; ?
+func CrackPassword(decoder *crypt.Decoder, hash string, maxLength int) (string, error) {
+	characterSet := "abcd"
+	characterSetLength := len(characterSet)
+
+	for length := 1; length <= maxLength; length++ {
+		total := 1
+
+		for i := 0; i < length; i++ {
+			total *= characterSetLength
+		}
+
+		for i := 0; i < total; i++ {
+			combination := make([]byte, length)
+			num := i
+
+			for pos := length - 1; pos >= 0; pos-- {
+				combination[pos] = characterSet[num%characterSetLength]
+				num /= characterSetLength
+			}
+
+			testPassword := string(combination)
+			if checkPassword(decoder, testPassword, hash) {
+				return testPassword, nil
+			}
+		}
+	}
+
+	return "", errors.New("failed to crack the password")
 }
