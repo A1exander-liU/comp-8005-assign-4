9ad26bea89263a0b0e656564efd1e5f8e71cf60a
A1exander-liU <alexanderliu2016@gmail.com>
2026-01-26 16:21:39 -0800

    refactor: split controller code

---
+++ cmd/controller/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/A1exander-liU/comp-8005-assign-1/internal/controller"
+	"github.com/A1exander-liU/comp-8005-assign-1/internal/shared"
+	"go.uber.org/zap"
+)
+
+func handleArguments(config *controller.Config) {
+	if config.Shadowfile == "" {
+		fmt.Println("Error: -f is required")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if config.Username == "" {
+		fmt.Println("Error: -u is required")
+		flag.Usage()
+		os.Exit(1)
+	}
+	if config.Port < 1 || config.Port > 65535 {
+		fmt.Println("Error: -p is required and must be in range: 1 - 65535 (inclusive)")
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func parseShadowfile(shadowfile, username string) shared.ShadowData {
+	contents, err := os.ReadFile(shadowfile)
+	if err != nil {
+		fmt.Println("Failed to read shadowfile:", err)
+		os.Exit(1)
+	}
+
+	entries := strings.SplitSeq(string(contents), "\n")
+	for entry := range entries {
+		user, hash, found := strings.Cut(entry, ":")
+		if !found {
+			continue
+		}
+
+		if user != username {
+			continue
+		}
+
+		shadow, err := shared.ParseHash(hash)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		shadow.Username = user
+		return shadow
+	}
+
+	fmt.Println("Failed to find user:", username)
+	os.Exit(1)
+
+	return shared.ShadowData{}
+}
+
+func parseArguments() controller.Config {
+	var config controller.Config
+
+	flag.StringVar(&config.Shadowfile, "f", "", "path to shadowfile")
+	flag.StringVar(&config.Username, "u", "", "username to be cracked")
+	flag.IntVar(&config.Port, "p", 0, "port number to listen on")
+
+	flag.Parse()
+
+	return config
+}
+
+func main() {
+	logger, _ := zap.NewDevelopment()
+	defer logger.Sync().Error()
+
+	config := parseArguments()
+	handleArguments(&config)
+
+	shadowData := parseShadowfile(config.Shadowfile, config.Username)
+	fmt.Println(shadowData)
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		fmt.Println("Failed to create logger:", err)
+		os.Exit(1)
+	}
+
+	controller := controller.NewController(logger)
+	controller.SetupServer(config.Port)
+
+	for {
+		conn, err := controller.AcceptConnection()
+		if err != nil {
+			controller.Logger.Info("Failed to accept connection", zap.Error(err))
+		}
+
+		go controller.HandleConnection(conn)
+	}
+}
---
+++ internal/controller/controller.go
@@ -0,0 +1,85 @@
+// Package controller contains class for controller
+package controller
+
+import (
+	"encoding/gob"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/A1exander-liU/comp-8005-assign-1/internal/shared"
+	"go.uber.org/zap"
+)
+
+type Config struct {
+	Shadowfile, Username string
+	Port                 int
+}
+
+// Controller is reponsible for managing worker connections
+// for sending and receiving password cracking jobs.
+type Controller struct {
+	Logger   *zap.Logger
+	listener net.Listener
+}
+
+// NewController creates a new Controller object with defaults:
+//
+// - A
+//
+// - B
+func NewController(logger *zap.Logger) *Controller {
+	return &Controller{
+		Logger: logger,
+	}
+}
+
+// SetupServer starts listening for connections on the specified port.
+//
+// This will exit with an error if listening failed.
+func (c *Controller) SetupServer(port int) {
+	address := fmt.Sprintf("[::]:%d", port)
+
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		fmt.Printf("Failed to listen on %s: %s\n", address, err)
+		os.Exit(1)
+	}
+
+	c.listener = listener
+
+	c.Logger.Info(fmt.Sprintf("Controller listening on %s", address))
+}
+
+// AcceptConnection accepts an incoming connection.
+//
+// This wraps around the net.Listener.Accept
+func (c *Controller) AcceptConnection() (net.Conn, error) {
+	return c.listener.Accept()
+}
+
+// HandleConnection manages communication with a single worker for the
+// whole entire lifecycle.
+func (c *Controller) HandleConnection(conn net.Conn) {
+	encoder := gob.NewEncoder(conn)
+	decoder := gob.NewDecoder(conn)
+
+	for {
+		var m shared.Message
+
+		if err := decoder.Decode(&m); err != nil {
+			c.Logger.Error("Failed to decode incoming message", zap.Error(err))
+			continue
+		}
+
+		c.Logger.Info("Received message",
+			zap.String("version", m.Version),
+			zap.String("message", m.Message),
+		)
+
+		_ = encoder.Encode(shared.Message{
+			Version: "1", Message: fmt.Sprintf("Received message: %s", m.Message),
+		})
+
+	}
+}
