5bfc56d8ed67ae54388e51470740b0af6b1d39cc
A1exander-liU <alexanderliu2016@gmail.com>
2026-01-26 19:36:23 -0800

    feat: add password data field

--- internal/shared/messages.go
+++ internal/shared/messages.go
@@ -1,7 +1,10 @@
 // Package shared contains data types and functions used between the controller and worker
 package shared
 
-const MessageVersion string = "1.0.0"
+const (
+	MessageVersion string = "1.0.0"
+	SearchSpace    string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#%^&*()_+-=.,:;?"
+)
 
 type MessageType string
 
@@ -17,8 +20,14 @@ type ShadowData struct {
 	Username, Algorithm, Parameters, Salt, Hash string
 }
 
+type PasswordData struct {
+	SearchSpace    string
+	PasswordLength int
+}
+
 type Message struct {
 	Type             MessageType
 	Version, Message string
 	Data             ShadowData
+	PasswordData     PasswordData
 }
